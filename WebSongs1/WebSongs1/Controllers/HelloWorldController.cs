22c8cda2a11ce6288301da3291bacc3dafc83bc0
splixble <dietrichneuman@gmail.com>
2024-01-23 11:54:25 -0800

    First WebFormsApp1 version

---
+++ WebSongs1/WebSongs1/Controllers/HelloWorldController.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace WebSongs1.Controllers
+{
+    public class HelloWorldController : Controller
+    {
+        // built according to tutorial at https://learn.microsoft.com/en-us/aspnet/mvc/overview/getting-started/introduction/adding-a-controller.
+        // 
+        // GET: /HelloWorld/ 
+
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+
+        // 
+        // GET: /HelloWorld/Welcome/ 
+
+        public ActionResult Welcome(string name, int numTimes)
+        {
+            ViewBag.Message = "Hello " + name;
+            ViewBag.NumTimes = numTimes;
+
+            return View();
+        }
+    }
+}
