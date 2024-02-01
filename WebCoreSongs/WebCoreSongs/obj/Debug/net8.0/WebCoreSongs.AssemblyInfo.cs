0744056e06c6ed6a92167513ece04968b4c7afb4
splixble <dietrichneuman@gmail.com>
2024-02-01 15:21:45 -0800

    WebCoreSongs: Finally brings up the Artists table!

--- WebCoreSongs/WebCoreSongs/obj/Debug/net8.0/WebCoreSongs.AssemblyInfo.cs
+++ WebCoreSongs/WebCoreSongs/obj/Debug/net8.0/WebCoreSongs.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("WebCoreSongs")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c37a53385290469f0d3240e19d6d905c75833189")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2c1200548027cdc918297250375ce661052ff79c")]
 [assembly: System.Reflection.AssemblyProductAttribute("WebCoreSongs")]
 [assembly: System.Reflection.AssemblyTitleAttribute("WebCoreSongs")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
