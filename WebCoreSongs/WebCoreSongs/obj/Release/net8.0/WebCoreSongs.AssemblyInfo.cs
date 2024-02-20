0c97d994fa077091e62a6d789411217a4b9b764c
splixble <dietrichneuman@gmail.com>
2024-02-19 16:47:27 -0800

    WebCoreSongs app seems to have been deployed on Azure...

--- WebCoreSongs/WebCoreSongs/obj/Release/net8.0/WebCoreSongs.AssemblyInfo.cs
+++ WebCoreSongs/WebCoreSongs/obj/Release/net8.0/WebCoreSongs.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("WebCoreSongs")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Release")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+06a2fd2a09547c14d930de4cbfbc23789c0d2d86")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+976360b1af485f874a12f099388dd87d12153ab1")]
 [assembly: System.Reflection.AssemblyProductAttribute("WebCoreSongs")]
 [assembly: System.Reflection.AssemblyTitleAttribute("WebCoreSongs")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
