1025debd6621d62d30d8281fda17f3d14d924860
splixble <dietrichneuman@gmail.com>
2024-02-22 13:38:51 -0800

    WebCoreSongs: generated Model for ViewSongPerformanceTotals
    ...but it threw an error when I tried to generate controllers and views

--- WebCoreSongs/WebCoreSongs/obj/Release/net8.0/WebCoreSongs.AssemblyInfo.cs
+++ WebCoreSongs/WebCoreSongs/obj/Release/net8.0/WebCoreSongs.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("WebCoreSongs")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Release")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+976360b1af485f874a12f099388dd87d12153ab1")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+3c6672733292ab6c05bb4ac2fa86367d27d54838")]
 [assembly: System.Reflection.AssemblyProductAttribute("WebCoreSongs")]
 [assembly: System.Reflection.AssemblyTitleAttribute("WebCoreSongs")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
