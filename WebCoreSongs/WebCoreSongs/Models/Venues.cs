5da312acc4206c3fba12c44bdaab29f1735fe15c
splixble <dietrichneuman@gmail.com>
2024-02-22 17:24:51 -0800

    Revert "WebCoreSongs: Last checkin before work on No Key problem"
    This reverts commit 75a730a3c1a385a28fe97613f86c46ed097eb559.

--- WebCoreSongs/WebCoreSongs/Models/Venues.cs
+++ WebCoreSongs/WebCoreSongs/Models/Venues.cs
@@ -2,14 +2,11 @@
 #nullable disable
 using System;
 using System.Collections.Generic;
-using System.ComponentModel.DataAnnotations; 
 
 namespace WebCoreSongs.Models;
 
 public partial class Venues
 {
-    // only put Key in, on 2/22/24, because it was saying the selectlist didn't have a key
-    [Key]
     public int Id { get; set; }
 
     public string Name { get; set; }
