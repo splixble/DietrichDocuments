8300eb8878d0551c914d00d84da9ad09a7ad32f6
splixble <dietrichneuman@gmail.com>
2024-03-01 17:14:26 -0800

    WCF: Published new version

--- WebCoreSongs/WebCoreSongs/obj/Debug/net8.0/WebCoreSongs.AssemblyInfo.cs
+++ WebCoreSongs/WebCoreSongs/obj/Debug/net8.0/WebCoreSongs.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("WebCoreSongs")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+45933b435b43aea0d8006896795bb7cbe8ab62fd")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+64251bb03c98361556cd06c81cd076bd392d0e32")]
 [assembly: System.Reflection.AssemblyProductAttribute("WebCoreSongs")]
 [assembly: System.Reflection.AssemblyTitleAttribute("WebCoreSongs")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
