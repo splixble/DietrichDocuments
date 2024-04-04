cd2d45907df990cd410fc84e1936dd0c917f784a
splixble <dietrichneuman@gmail.com>
2024-04-04 14:50:47 -0700

    WCS: Got typeahead ctrl for Venue fields pretty much working

--- WebCoreSongs/WebCoreSongs/obj/Debug/net8.0/WebCoreSongs.AssemblyInfo.cs
+++ WebCoreSongs/WebCoreSongs/obj/Debug/net8.0/WebCoreSongs.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("WebCoreSongs")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+719c4713b92c4effa2114b9c74a9e187a06c4dc6")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f59b5232e5e26d4189bcb2b16218dee0d9a26789")]
 [assembly: System.Reflection.AssemblyProductAttribute("WebCoreSongs")]
 [assembly: System.Reflection.AssemblyTitleAttribute("WebCoreSongs")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
